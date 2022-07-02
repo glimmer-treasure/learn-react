e8512900b13f61e1cc71689f1e01022e12a2f671
liang.cheng <liang.cheng@ttpai.cn>
2022-07-02 23:54:04 +0800

    feat：创建react项目

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import FormPage from './pages/FormPage'
+
+function App() {
+  return (
+    <div>
+      <FormPage />
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
