281f0e48cbe21501d1d155c3b799445ebb708f7a
liang.cheng <liang.cheng@ttpai.cn>
2022-07-06 22:22:05 +0800

    feat: 安装redux-thunk和redux-logger

--- src/pages/ReduxPage.js
+++ src/pages/ReduxPage.js
@@ -1,5 +1,7 @@
 import { useCallback, useEffect, useState } from 'react'
 import { applyMiddleware,createStore } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 // import { createStore } from '../components/Redux'
 import '../styles/Redux.css'
 
@@ -12,7 +14,7 @@ const reducer = (state={value: 0}, action) => {
     }
 }
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk, logger))
 
 const FormPage = (props) => {
     const state = store.getState()
@@ -20,13 +22,22 @@ const FormPage = (props) => {
     const increase = useCallback(() => {
         store.dispatch({type: 'increase'})
     })
+    const asyncIncrease = useCallback(() => {
+        store.dispatch((dispatch, getState) => {
+            const state = getState()
+            setTimeout(() => {
+                dispatch({type: 'increase', payload: state.value + 3})
+            }, 3000);
+        })
+    })
     useEffect(() => {
-        store.subscribe(() => forceUpdate(+new Date))
+        return store.subscribe(() => forceUpdate(+new Date))
     }, [])
     return (
         <div className="redux__container">
             <input className="redux__input" value={state.value} />
-            <button className="redux__btn--add-btn" onClick={increase}>同步加</button>
+            <button className="redux__btn redux__btn--add-btn" size='large' onClick={increase}>同步加</button>
+            <button className="redux__btn redux__btn--add-btn" onClick={asyncIncrease}>异步加</button>
         </div>
     )
 }
